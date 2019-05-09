893b366c0619bb3de7f9c10b9ce75d02fad9502d
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-05-09 19:51:34 +0300

    RSS add class"Channel".

---
+++ app/js/Channel.js
@@ -0,0 +1,17 @@
+import { url } from "inspector";
+
+/* 
+    Describes the structure of the object 'Channel'
+*/
+
+export default class Channel{
+
+
+    constructor(nameChannel, urlChannel){
+        
+        this.nameChannel=nameChannel;
+        this.urlChannel=urlChannel;
+
+    }
+
+}
