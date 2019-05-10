3f61d6ce3f47ac75678315e153981cd04b763093
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-05-10 19:03:27 +0300

    RSS  AddHandler.js : work on adding

--- app/js/AddHandler.js
+++ app/js/AddHandler.js
@@ -19,10 +19,47 @@ export default class AddHandler{
             let channel=that.channelObj;
             channel.nameChannel=document.getElementById('nameChannel').value;
             channel.urlChannel=document.getElementById('urlChannel').value;
-            
-            let localStorage=that.storageObj;
-            localStorage.addNewCannel(channel);
 
+            if(channel.nameChannel==''){
+
+                alert('write name of chanel, please');
+
+            }else if (channel.urlChannel==''){
+
+                alert('write url of chanel, please');
+
+            }else{
+
+                let localStorage=that.storageObj;
+                let idChannel=localStorage.listChannels().length;
+                localStorage.addNewCannel(channel);
+
+                let list=document.getElementById('list');
+
+                let tr=document.createElement('tr');
+
+                let tdChannelName=document.createElement('td');
+                
+                let tdReadChannel=document.createElement('td');
+                let buttonRead=document.createElement('button');
+                buttonRead.innerText='read channel';
+                tdReadChannel.appendChild(buttonRead);
+
+                let tdBtnDelete=document.createElement('td');
+                let buttonDelete=document.createElement('button');
+                buttonDelete.innerText='delete';
+                tdBtnDelete.appendChild(buttonDelete);
+                
+                tdChannelName.innerText=channel.nameChannel;
+                tr.appendChild(tdChannelName);
+                tr.appendChild(tdReadChannel);
+                tr.appendChild(tdBtnDelete);
+                list.appendChild(tr);
+
+                document.getElementById('nameChannel').value='';
+                document.getElementById('urlChannel').value='';
+            };
+            
         };
 
     };
