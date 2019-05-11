de2ebcd46082d4a0c3a26d4dfb8cd8ae434cc061
NovoseletskiyVadim <novoseletskiy.v@gmail.com>
2019-05-11 14:19:12 +0300

    RSS import DeleteHandler and ChannelListView in main.js

--- app/js/main.js
+++ app/js/main.js
@@ -4,12 +4,26 @@ console.log('main is ok ');
 import Channel from '../js/Channel.js';
 import ChannelLocalStorage from '../js/ChannelLocalStorage.js';
 import AddHandler from '../js/AddHandler.js';
+import DeleteHandler from '../js/deleteHandler.js';
+import ChannelListView from '../js/ChannelListView.js';
 
 let channel=new Channel();
 let storage= new ChannelLocalStorage();
-storage.listChannels();
 
+storage.listChannels();
+let arr=storage.listChannels();
+
+let view =new ChannelListView(arr);
+view.viewChannelList();
 
 let add=new AddHandler(channel,storage);
 add.addChannel();
+
+let deleteChannel = new DeleteHandler(storage,view);
+deleteChannel.deleteChannel();
+
+
+
+
+
 
